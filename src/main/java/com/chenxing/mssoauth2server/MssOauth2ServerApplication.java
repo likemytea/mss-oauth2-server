b4479e6cb39a533d779504397f4e0d505f7920f1
huayu <huayu@DESKTOP-N5K2GI1>
2018-09-04 10:27:36 +0800

    aaaaaa

---
+++ src/main/java/com/chenxing/mssoauth2server/MssOauth2ServerApplication.java
@@ -0,0 +1,12 @@
+package com.chenxing.mssoauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssOauth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssOauth2ServerApplication.class, args);
+	}
+}
